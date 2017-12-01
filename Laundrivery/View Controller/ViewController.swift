ee633cd2fe73ccff5320a393249df87b14d18429
Difa Sanditya <me@difasanditya.com>
2017-12-01 19:12:08 +0700

    Created storyboard for welcome page and added tab bar controller

--- Laundrivery/View Controller/ViewController.swift
+++ Laundrivery/View Controller/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore  {
+            print("go to next")
+        } else {
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
